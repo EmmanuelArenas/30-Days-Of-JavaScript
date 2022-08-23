a1436b671eda68d88dbed1ddff6c3a94b1030842
EmmanuelArenas <emmanuel.live19@gmail.com>
2022-08-22 21:12:31 -0500

    day 1 of 30 finished

--- 01_Day_Introduction/01_day_starter/main.js
+++ 01_Day_Introduction/01_day_starter/main.js
@@ -1,4 +1,20 @@
 // the variable values can be accessed from other variable.js file
-console.log(firstName, lastName, country, city, age, isMarried) 
-console.log(gravity, boilingPoint, PI) // 9.81, 100, 3.14
-console.log(name, job, live)
+console.log(firstName, lastName, country, city, age, isMarried);
+console.log(gravity, boilingPoint, PI); // 9.81, 100, 3.14
+console.log(name, job, live);
+
+// 1.-coments can make code readable
+// 2.-Welcome to 30DaysOfJavaScript
+/* 
+3.- Comments can make code readable, 
+easy to reuse and informative 
+*/
+
+// 4.- Declaring different variables of different data types
+let firstName = "Emmanuel";
+let lastName = "Arenas";
+let age = 25; //years
+let height = 1.63; //height in meters
+let isMarried = false;
+let car = null;
+let originOfHisLastName = undefined;
--- 01_Day_Introduction/variable.js
+++ 01_Day_Introduction/variable.js
@@ -1,17 +1,62 @@
 // Declaring different variables of different data types
-let firstName = 'Asabeneh' // first name of a person
-let lastName = 'Yetayeh' // last name of a person
-let country = 'Finland' // country
-let city = 'Helsinki' // capital city
-let age = 100 // age in years
-let isMarried = true
+// let firstName = 'Asabeneh' // first name of a person
+// let lastName = 'Yetayeh' // last name of a person
+// let country = 'Finland' // country
+// let city = 'Helsinki' // capital city
+// let age = 100 // age in years
+// let isMarried = true
 
 // Declaring variables with number values
-const gravity = 9.81 // earth gravity  in m/s2
-const boilingPoint = 100 // water boiling point, temperature in oC
-const PI = 3.14 // geometrical constant
+const gravity = 9.81; // earth gravity  in m/s2
+const boilingPoint = 100; // water boiling point, temperature in oC
+const PI = 3.14; // geometrical constant
 
 // Variables can also be declaring in one line separated by comma
-let name = 'Asabeneh', //name of a person
-  job = 'teacher',
-  live = 'Finland'
+let name = "Asabeneh", //name of a person
+  job = "teacher",
+  live = "Finland";
+
+//5.-use the JavaScript typeof operator to check different data types. Check the data type of each variable
+console.log(typeof "Emmanuel");
+console.log(typeof "Arenas");
+console.log(typeof 25);
+console.log(typeof 1.63);
+console.log(typeof false);
+console.log(typeof null);
+console.log(typeof undefined);
+
+//6.- Declare four variables without assigning values
+let first;
+let second;
+let third;
+let quater;
+
+// 7.- Declare four variables with assigned values
+
+let one = "Luis";
+let two = 25;
+let three = 1.63;
+let four = true;
+
+// 8.- Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+let firstNam = "Emmanuel";
+let lastNam = "Arenas";
+let isMarried = false;
+let countr = "México";
+let agee = 31;
+
+// 9.- Declare variables to store your first name, last name, marital status, country and age in a single line
+
+let firstName = "Luis",
+  lastName = "Smith",
+  married = true,
+  country = "Alemania",
+  age = 21;
+
+// 10 Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+let myAge = 31;
+let yourAge = 49;
+
+console.log(`I'm ${myAge} years old. 
+You are ${yourAge} years old.`);
